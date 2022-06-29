67535fa49e1e5d47a5a0c20a8c659e91989e0c3e
Milo Tischler <milo.tischler@gmail.com>
2022-06-29 18:24:17 +0200

    Added Expo Face Detector

--- screens/TalkScreen.tsx
+++ screens/TalkScreen.tsx
@@ -1,13 +1,46 @@
-import { Heading, Text, View, VStack } from 'native-base';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, VStack, HStack, Box, Button } from 'native-base';
 import { RootTabScreenProps } from '../types';
+import { Camera, CameraType } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
+import { StyleSheet } from 'react-native';
+
+// @todo try to use https://github.com/tensorflow/tfjs-models/tree/master/face-landmarks-detection/src/mediapipe ???
+// @todo or maybe use https://github.com/nonth/react-native-face-detection
 
 export default function TalkScreen ({ navigation }: RootTabScreenProps<'Talk'>) {
+  const handleFacesDetected = ({ faces }) => {
+    if (faces.length) {
+      const face = faces[0];
+
+      if (face.rightEyeOpenProbability < 0.4) {
+        console.log('-------------> right eye blinked!!!!', face.rightEyeOpenProbability);
+      } else {
+        console.log(face.rightEyeOpenProbability);
+      }
+    }
+  };
+
   return (
-    <View justifyContent="center" flex="1">
-      <VStack space={1} alignItems="center">
-        <Heading size="lg">Todo..</Heading>
-        <Text>Screen to loop through letters and watch for blink to capture input.</Text>
-      </VStack>
+    <View flex="1">
+      <Camera
+        style={styles.camera}
+        type={CameraType.front}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+          runClassifications: FaceDetector.FaceDetectorClassifications.all,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}>
+      </Camera>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  }
+});
